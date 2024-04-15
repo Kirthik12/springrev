b785046c792a34d53e63812d51a41df638d985a7
Kirthik <118422169+Kirthik12@users.noreply.github.com>
2024-04-15 20:42:15 +0530

    Add files via upload

--- pharmacy/src/main/java/com/pharm/pharmacy/model/Inventory.java
+++ pharmacy/src/main/java/com/pharm/pharmacy/model/Inventory.java
@@ -1,29 +1,25 @@
 package com.pharm.pharmacy.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
 @NoArgsConstructor
-public class User {
+@Entity
+public class Inventory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String userName;
-    private String password;
-    
-    @OneToOne
-    @JoinColumn(name="I_id")
-    private Inventory inventory;
-    
+
+    private Long I_id;
+    private String Medicine;
+    private int price;
+
+    @OneToOne(mappedBy = "inventory",cascade = CascadeType.ALL)
+    private User user;
 }
